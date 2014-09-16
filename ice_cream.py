0833f0bcda7c7ae649343fcfd076505b0ebb41b7
ym26 <ym26@nyu.edu>
2014-09-15 23:23:54 -0400

    synopsis

---
+++ ice_cream.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# As the new semester begins, the department is offering free ice cream
+# for every student, but they don't want any students have more than 1 ice cream,
+# because there is limited amount of ice cream. However, bad thing happens. There is
+# a student, who managed to eat 2 ice creams. The department wants you to design
+# an algorithm to find the name of the student, and in return you will get an extra ice
+# cream. Given an array of names your job is to find the name of the student, who had
+# 2 ice creams. Give the worst case running time of your algorithm using big-Theta notation. (You
+# may use any algorithm presented in class.)
+
+
+
